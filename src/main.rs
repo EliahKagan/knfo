c350d7ca3f077d29e13a2ad82d707665b8fae9ff
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-07 05:50:39 -0400

    Start on listing known folders
    This currently starts to list them but fails with an:
    
        Error { code: HRESULT(0x80070002), message: "The system cannot find the file specified." }
    
    Although I'm not entirely certain why, this makes sense because
    maybe some known folders aren't actually present and thus may not
    have paths.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,107 @@
-fn main() {
-    println!("Hello, world!");
+//! List the special "known folders" on a Windows system, and their locations.
+//!
+//! See [Known Folders](https://learn.microsoft.com/en-us/windows/win32/shell/known-folders).
+
+use windows::{
+    core::{Error, GUID, PWSTR},
+    Win32::{
+        System::Com::{
+            CoCreateInstance, CoInitializeEx, CoTaskMemFree, CoUninitialize, CLSCTX_INPROC_SERVER,
+            COINIT_APARTMENTTHREADED,
+        },
+        UI::Shell::{IKnownFolder, IKnownFolderManager, KnownFolderManager, KF_FLAG_DEFAULT, KNOWNFOLDER_DEFINITION},
+    },
+};
+
+struct ComInit;
+
+impl ComInit {
+    fn new() -> Result<Self, Error> {
+        let hresult = unsafe { CoInitializeEx(None, COINIT_APARTMENTTHREADED) };
+        if hresult.is_err() {
+            Err(Error::from_hresult(hresult))
+        } else {
+            Ok(ComInit)
+        }
+    }
+}
+
+impl Drop for ComInit {
+    fn drop(&mut self) {
+        unsafe { CoUninitialize() }
+    }
+}
+
+struct KnownFolderIds {
+    pkfid: *mut GUID,
+    count: u32,
+}
+
+impl KnownFolderIds {
+    fn new(kf_manager: &IKnownFolderManager) -> Result<Self, Error> {
+        let mut pkfid = std::ptr::null_mut();
+        let mut count = 0;
+        unsafe { kf_manager.GetFolderIds(&mut pkfid, &mut count)? };
+        Ok(KnownFolderIds { pkfid, count })
+    }
+
+    fn as_slice(&self) -> &[GUID] {
+        unsafe { std::slice::from_raw_parts(self.pkfid, self.count as usize) }
+    }
+}
+
+impl Drop for KnownFolderIds {
+    fn drop(&mut self) {
+        unsafe { CoTaskMemFree(Some(self.pkfid as *const _)) }
+    }
+}
+
+struct KnownFolderDefinition {
+    fields: KNOWNFOLDER_DEFINITION,
+}
+
+impl KnownFolderDefinition {
+    fn of(folder: &IKnownFolder) -> Result<Self, Error> {
+        let mut fields = KNOWNFOLDER_DEFINITION::default();
+        unsafe { folder.GetFolderDefinition(&mut fields)? };
+        Ok(KnownFolderDefinition { fields })
+    }
+}
+
+fn co_free_pwstr(pwstr: PWSTR) {
+    unsafe { CoTaskMemFree(Some(pwstr.as_ptr() as *const _)) };
+}
+
+impl Drop for KnownFolderDefinition {
+    fn drop(&mut self) {
+        // The windows crate does not provide FreeKnownFolderDefinitionFields, possibly
+        // due to it being an __inline function. This frees each of the fields that is a
+        // pointer to a string, which is equivalent to FreeKnownFolderDefinitionFields.
+        co_free_pwstr(self.fields.pszName);
+        co_free_pwstr(self.fields.pszDescription);
+        co_free_pwstr(self.fields.pszRelativePath);
+        co_free_pwstr(self.fields.pszParsingName);
+        co_free_pwstr(self.fields.pszTooltip);
+        co_free_pwstr(self.fields.pszLocalizedName);
+        co_free_pwstr(self.fields.pszIcon);
+        co_free_pwstr(self.fields.pszSecurity);
+    }
+}
+
+fn main() -> Result<(), Error> {
+    unsafe {
+        let _com = ComInit::new()?;
+
+        let kf_manager: IKnownFolderManager =
+            CoCreateInstance(&KnownFolderManager, None, CLSCTX_INPROC_SERVER)?;
+
+        for id in KnownFolderIds::new(&kf_manager)?.as_slice() {
+            let folder = kf_manager.GetFolder(id)?;
+            let name = KnownFolderDefinition::of(&folder)?.fields.pszName.to_string()?;
+            let path = folder.GetPath(0)?.to_string()?; // FIXME: Use KF_FLAG_DEFAULT if possible.
+            println!("{name}: {path}");
+        }
+
+        Ok(())
+    }
 }
